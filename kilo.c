2d4b4b556d218880b7b3382c0ee9a9a0f9b20122
Yacine Abed <yacine.abed@student.univ-temouchent.edu.dz>
2025-06-20 15:05:16 +0100

    disable more local, input and output terminal flags for raw mode

--- kilo.c
+++ kilo.c
@@ -15,10 +15,20 @@ void enableRawMode() {
   atexit(disableRawMode);
 
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);   //invert the echo bit (disable)
+
+  raw.c_iflag &= ~(ICRNL | IXON);
+  // Disable Ctrl-S and Ctrl-Q (software flow control)
+  // Disable carriage return to newline translation (fixes Ctrl-M)
+  //
+  raw.c_oflag &= ~(OPOST);  // disable translating '\n' to '\r\n' in the output
+
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);   //disable echo, canonical mode, ctrl-v and signals
+
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
-                        //TCSAFLUSH makes sure that leftover input is no longer
-                        //fed into your shell after the program quits
+  // Disable echo (ECHO)
+  // Disable canonical mode (ICANON), so input is read byte-by-byte
+  // Disable Ctrl-V (IEXTEN) — for literal input mode
+  // Disable signal keys like Ctrl-C and Ctrl-Z (ISIG)
 }
 
 int main() {
@@ -30,9 +40,9 @@ int main() {
 
         if(iscntrl(c)) {
 
-            printf("%c\n", c);
+            printf("%c\r\n", c);
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     return 0;
