2afc0cdb9e59e8763698ba0a935b2845aa352a19
Yacine Abed <yacine.abed@student.univ-temouchent.edu.dz>
2025-06-21 10:56:51 +0100

    edit error and ctrl-Q handlers to include clearing the screen and repositioning the cursor

--- kilo.c
+++ kilo.c
@@ -16,8 +16,11 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
-  perror(s);
-  exit(1);
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    perror(s);
+    exit(1);
 }
 
 void disableRawMode() {
@@ -80,6 +83,8 @@ void editorProcessKeypress() {
 
     switch(c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
     }
