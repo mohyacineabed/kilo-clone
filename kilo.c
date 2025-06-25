8aad02e80bd11ef910f49a771804d78451a8e52c
Yacine Abed <yacine.abed@student.univ-temouchent.edu.dz>
2025-06-25 15:25:17 +0100

    improve screen redraw

--- kilo.c
+++ kilo.c
@@ -138,6 +138,7 @@ void editorDrawRows(struct abuf *ab) {
     for (y = 0; y < E.screenrows; y++) {
         abAppend(ab, "~", 1);
 
+        abAppend(ab, "\x1b[K", 4); // clears text from cursor to end of line
         if (y < E.screenrows - 1) {
             abAppend(ab, "\r\n", 2);
         }
@@ -147,14 +148,14 @@ void editorDrawRows(struct abuf *ab) {
 void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
 
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6); // hide the cursor
     abAppend(&ab, "\x1b[H", 3);
-    //write esc char "\x1b" followed by '[' and "2J" to clear the screen
-    //reposition the cursor to top of the screen ("<esc>[Row ; Col H")
+    //reposition the cursor to top of the screen ("<esc>[Row;Col H")
 
     editorDrawRows(&ab);
 
     abAppend(&ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[?25h", 6); // show the cursor
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);
